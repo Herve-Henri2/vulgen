09826402f0662c9de5e0941a5888978ca110987a
Zheitoune <idinbak@gmail.com>
2022-12-10 20:37:35 +0100

    Small changes

--- docker_images/log4shell/create_img.sh
+++ docker_images/log4shell/create_img.sh
@@ -1,10 +1,10 @@
-#! /usr/bin/bash
+#! /bin/bash
 
 # Retrieve this script directory
 DIR_NAME=$(dirname ${BASH_SOURCE:-$0})
 
 # Delete all containers
-docker rm $(docker ps -a -q)
+#docker rm $(docker ps -a -q)
 
 # Retrieve id of old image
 old_img=$(docker images | grep $DIR_NAME)
@@ -19,7 +19,7 @@ id=$(grep -Fxf tmp1.txt tmp2.txt)
 rm tmp1.txt tmp2.txt
 
 # Delete old image
-docker rmi $id
+docker rmi -f $id
 
 # Create the new image
 docker image build -t $DIR_NAME:custom $DIR_NAME
